34c3acf3be6bd3bdbb7b00f39ea098cba7421396
Ricardo Cardona Lafon <cardonalafon@gmail.com>
2017-10-03 11:04:32 -0300

    Fin del Proyecto - solo PHP - Orientó Enrique Pallares
    Gracias a Enrique Pallares que me orientó que el sitio no estaba actualizado y había que hacer uno de los dos proyectos en PHP o NODE... Entrego el Proyecto en PHP finalizado hace un par de días.

--- NextUcodBaseAgenda_versionNode/codBase/bd/CRUD.js
+++
@@ -1,8 +0,0 @@
-module.exports.insertarRegistro = function (db, callback){
-    let coleccion = db.collection("users")
-    coleccion.insertMany([
-        {Usuario: "Ricardo", Passw: 12345}
-    ],(error,result)=>{
-        console.log("Resultado de insert: " + result.toString())
-    })
-}
